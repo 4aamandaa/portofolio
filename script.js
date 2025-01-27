0ee8fb291acb932e64c3d035507ec57985816c2b
Amanda's <aamandaemel@gmail.com>
2025-01-27 19:37:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Ketika Humberger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik diluar sidebar untuk menghilangkan Nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// ICON DARK THEME
+var icon = document.getElementById("icon");
+
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "img/light.png";
+  } else {
+    icon.src = "img/dark.png";
+  }
+};
+
+// Untuk Pembersihan Formspree
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName('form')) {
+    form.reset();
+  }
+}
